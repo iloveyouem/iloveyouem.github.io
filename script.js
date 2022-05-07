66dd28abfe18acb69d0f4606d9dda0a260e65d96
time-together <105085003+time-together@users.noreply.github.com>
2022-05-07 20:26:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     // Month Day, Year Hour:Minute:Second, id-of-element-container
-    countUpFromTime("May 9, 2021 20:00:00", 'time'); // ****** Change this line!
+    countUpFromTime("May 9, 2021 21:00:00", 'time'); // ****** Change this line!
   };
   function countUpFromTime(countFrom, id) {
     countFrom = new Date(countFrom).getTime();
@@ -18,13 +18,42 @@ window.onload = function() {
     mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
     secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
   
+    if (years == 1) {yearp = " year";} else {yearp = " years";}
+    if (days == 1) {dayp = " year";} else {dayp = " days";}
+    if (hours == 1) {hourp = " hour";} else {hourp = " hours";}
+    if (mins == 1) {minp = " minute";} else {minp = " minutes";}
+    if (secs == 1) {secp = " second";} else {secp = " seconds";}
+
     var idEl = document.getElementById(id);
-    idEl.getElementsByClassName('years')[0].innerHTML = years;
-    idEl.getElementsByClassName('days')[0].innerHTML = days - (365*years);
-    idEl.getElementsByClassName('hours')[0].innerHTML = hours;
-    idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
-    idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+    idEl.getElementsByClassName('years')[0].innerHTML = years + yearp;
+    idEl.getElementsByClassName('days')[0].innerHTML = (days - (365 * years)) + dayp;
+    idEl.getElementsByClassName('hours')[0].innerHTML = hours + hourp;
+    idEl.getElementsByClassName('minutes')[0].innerHTML = mins + minp;
+    idEl.getElementsByClassName('seconds')[0].innerHTML = secs + secp;
   
     clearTimeout(countUpFromTime.interval);
     countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
   }
+
+  function moveScroller() {
+    var $anchor = $("#scroller-anchor");
+    var $scroller = $('#scroller');
+
+    var move = function() {
+        var st = $(window).scrollTop();
+        var ot = $anchor.offset().top;
+        if(st > ot) {
+            $scroller.css({
+                position: "fixed",
+                top: "0px"
+            });
+        } else {
+            $scroller.css({
+                position: "relative",
+                top: ""
+            });
+        }
+    };
+    $(window).scroll(move);
+    move();
+}
